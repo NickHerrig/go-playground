54a6dbea6bb4a80643a0dfe51e3ef8b1534eed1e
nick <neherrig@gmail.com>
2020-03-26 03:06:06 +0000

    Add a show snippet and create snippet route and servemux

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Hello from Snippetbox"))
 }
 
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Show some Snippet"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Create some Snippet"))
+}
+
 func main() {
     mux := http.NewServeMux()
     mux.HandleFunc("/", home)
+    mux.HandleFunc("/snippet", showSnippet)
+    mux.HandleFunc("/snippet/create", createSnippet)
 
     log.Println("starting server on port :4000")
     err := http.ListenAndServe(":4000", mux)
