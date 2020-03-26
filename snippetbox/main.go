295e68f286c817bf8e41ab08b441d6a7685b1783
nick <neherrig@gmail.com>
2020-03-26 21:43:52 +0000

    Add url query string for snippet route

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+    "fmt"
     "log"
     "net/http"
+    "strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +17,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Show some Snippet"))
+    id, err := strconv.Atoi(r.URL.Query().Get("id"))
+    if err != nil || id < 1 {
+        http.NotFound(w, r)
+        return
+    }
+
+    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
