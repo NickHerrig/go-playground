319af1793a2da2c2504158af24402fffdde8b65f
nickherrig <neherrig@gmail.com>
2020-03-28 14:57:17 -0500

    Add error and info logging, custom http struct

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -4,12 +4,16 @@ import (
     "flag"
     "log"
     "net/http"
+    "os"
 )
 
 func main() {
     addr := flag.String("addr", ":4000", "HTTP network address")
 
     flag.Parse()
+
+    infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+    errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
 
     mux := http.NewServeMux()
     mux.HandleFunc("/", home)
@@ -20,7 +24,13 @@ func main() {
 
     mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-    log.Printf("starting server on %s", *addr)
-    err := http.ListenAndServe(*addr, mux)
-    log.Fatal(err)
+    srv := &http.Server{
+        Addr: *addr,
+        ErrorLog: errorLog,
+        Handler: mux,
+    }
+
+    infoLog.Printf("starting server on %s", *addr)
+    err := srv.ListenAndServe()
+    errorLog.Fatal(err)
 }
