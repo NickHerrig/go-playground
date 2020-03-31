114fad733ff99a41f56134843ee0206fc2370b45
nick <neherrig@gmail.com>
2020-03-31 23:57:09 +0000

    Add mysql database, dsn flagg, and db driver

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+    "database/sql"
     "flag"
     "log"
     "net/http"
     "os"
+
+    _ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,11 +17,20 @@ type application struct {
 
 func main() {
     addr := flag.String("addr", ":4000", "HTTP network address")
+    dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 
     flag.Parse()
 
     infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
     errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
+    db, err := openDB(*dsn)
+    if err != nil {
+        errorLog.Fatal(err)
+    }
+
+    defer db.Close()
+
 
     app := &application{
         errorLog: errorLog,
@@ -32,6 +44,17 @@ func main() {
     }
 
     infoLog.Printf("starting server on %s", *addr)
-    err := srv.ListenAndServe()
+    err = srv.ListenAndServe()
     errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+        return nil, err
+    }
+    if err = db.Ping(); err != nil {
+        return nil, err
+    }
+    return db, nil
 }
--- snippetbox/go.mod
+++ snippetbox/go.mod
@@ -1,3 +1,5 @@
 module nickherrig.com/snippetbox
 
 go 1.14
+
+require github.com/go-sql-driver/mysql v1.5.0
