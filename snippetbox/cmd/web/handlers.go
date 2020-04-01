eccec4765998bd646c5b81691cdc30164594271c
nick <neherrig@gmail.com>
2020-04-01 18:00:47 +0000

    Using snippets sql querie to add to database through create dummy data

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -48,5 +48,15 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    w.Write([]byte("Create some Snippet"))
+    title :=  "dummy title data"
+    content :=  "dummy content data"
+    expires := "7"
+
+    id, err := app.snippets.Insert(title, content, expires)
+    if err != nil {
+        app.serverError(w, err)
+        return
+    }
+
+    http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
--- snippetbox/pkg/models/mysql/snippets.go
+++ snippetbox/pkg/models/mysql/snippets.go
@@ -10,8 +10,21 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-    return 0, nil
-}
+    stmt := `INSERT INTO snippets (title, content, created, expires)
+    VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+    result, err := m.DB.Exec(stmt, title, content, expires)
+    if err != nil {
+        return 0, err
+    }
+
+    id, err := result.LastInsertId()
+    if err != nil {
+        return 0, err
+    }
+
+    return int(id), nil
+ }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
     return nil, nil
