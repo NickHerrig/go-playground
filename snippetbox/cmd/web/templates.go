a5e402cf4199d1abb697c2109537568a3d244e3e
nick <neherrig@gmail.com>
2020-04-05 23:39:17 +0000

    Add custome template functions

--- snippetbox/cmd/web/templates.go
+++ snippetbox/cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
     "html/template"
     "path/filepath"
+    "time"
 
     "nickherrig.com/snippetbox/pkg/models"
 )
@@ -11,6 +12,14 @@ type templateData struct {
     CurrentYear int
     Snippet     *models.Snippet
     Snippets    []*models.Snippet
+}
+
+func humanDate(t time.Time) string{
+    return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+    "humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -24,7 +33,7 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
     for _, page := range pages {
         name := filepath.Base(page)
 
-        ts, err := template.ParseFiles(page)
+        ts, err := template.New(name).Funcs(functions).ParseFiles(page)
         if err != nil {
             return nil, err
         }
