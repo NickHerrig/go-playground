87019e3e8aab5237678e18d485676bd024584bb4
nick <neherrig@gmail.com>
2020-04-05 22:28:24 +0000

    Use stings.HasPrefix to add https to URL.

---
+++ go-book/ch_one_tutorial/ex_one_eight.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+    "io"
+    "net/http"
+    "os"
+    "strings"
+)
+
+func main() {
+
+    for _, url := range os.Args[1:] {
+        if ! strings.HasPrefix(url, "https://") {
+            url = "https://" + url
+        }
+
+        resp, err := http.Get(url)
+
+        if err != nil {
+            fmt.Fprint(os.Stderr, err)
+            os.Exit(1)
+        }
+        _, err = io.Copy(os.Stdout, resp.Body)
+        if err != nil {
+            fmt.Fprint(os.Stderr, err)
+            os.Exit(1)
+        }
+    }
+}
