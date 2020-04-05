81bf77217532486ae9a5b6be5867d16b2db2a8a3
nick <neherrig@gmail.com>
2020-04-05 22:52:45 +0000

    Add buffer for template files to handle runtime template errors

--- snippetbox/cmd/web/helpers.go
+++ snippetbox/cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import(
+    "bytes"
     "fmt"
     "net/http"
     "runtime/debug"
@@ -28,8 +29,12 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
         return
     }
 
-    err := ts.Execute(w, td)
+    buf := new(bytes.Buffer)
+
+    err := ts.Execute(buf, td)
     if err != nil {
         app.serverError(w, err)
+        return
     }
+    buf.WriteTo(w)
 }
