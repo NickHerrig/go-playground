048e7357751b6674033bcbee33f2f9fd9782c5c2
nickherrig <neherrig@gmail.com>
2020-10-13 09:54:11 -0500

    Add return error for grettings module

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -2,10 +2,14 @@ package greetings
 
 import (
     "fmt"
+    "errors"
 )
 
 
-func Hello(name string) string{
+func Hello(name string) (string, error) {
+    if name == "" {
+        return "", errors.New("empty name")
+    }
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+    return message, nil
 }
