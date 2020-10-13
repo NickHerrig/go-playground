2f0ff987ee5fe1bf15e3b045aa663f344b66f0eb
nickherrig <neherrig@gmail.com>
2020-10-13 11:10:49 -0500

    Add random greeting, and init() func

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
     "fmt"
     "errors"
+    "time"
+    "math/rand"
 )
 
 
@@ -10,6 +12,20 @@ func Hello(name string) (string, error) {
     if name == "" {
         return "", errors.New("empty name")
     }
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    message := fmt.Sprintf(randomFormat(), name)
     return message, nil
+}
+
+func init() {
+    rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+    formats := []string{
+        "Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
+
+    return formats[rand.Intn(len(formats))]
 }
