c9844dd964dd14031ea3ee62d314e06593c6509e
nickherrig <neherrig@gmail.com>
2020-10-14 17:55:10 -0500

    Add test for errors

--- greetings/greetings_test.go
+++ greetings/greetings_test.go
@@ -13,3 +13,10 @@ func TestHelloName(t *testing.T) {
         t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
     }
 }
+
+func TestHelloEmpty(t *testing.T) {
+    msg, err := Hello("")
+    if msg != "" || err == nil {
+        t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+    }
+}
