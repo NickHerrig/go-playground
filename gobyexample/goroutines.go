3cc8b8ba2ac0048ac244e4b317b799e581ebcbd4
nickherrig <neherrig@gmail.com>
2021-10-14 20:59:22 -0500

    Goroutines go-by-example

---
+++ gobyexample/goroutines.go
@@ -0,0 +1,25 @@
+package main
+
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func f(from string) {
+	for i := 0; i < 3; i++{
+		fmt.Println(from, ":", i)
+	}
+}
+
+
+
+func main(){
+	f("direct")
+	go f("concurrent")
+	go f("again")
+	go f("now")
+	time.Sleep(time.Second*5)
+
+}
